d418c60ad65c8117817bcf5e441617aecc1e3320
Thoriq Pratamaa <helloikha@gmail.com>
2023-01-16 16:57:52 +0700

    Chore : Merging Branch, Resize PC Size, Final

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
       phone: "320px",
-      pc: "1920px",
+      pc: "1080px",
     },
     extend: {
       fontFamily: {
