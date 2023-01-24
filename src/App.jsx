7b22fb9116c2afe64674cc3aad782a00d077e05a
riioaditya <rioadityano@gmail.com>
2023-01-24 15:40:09 +0700

    refactor: semantic change on importing file

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
-import React from "react"
+import React from "react";
 
 //Fonts
-import "../src/fonts/KGHAPPY.ttf"
-import "../src/fonts/KGHAPPYShadows.ttf"
-import "../src/fonts/KGHAPPYSolid.ttf"
+import "../src/fonts/KGHAPPY.ttf";
+import "../src/fonts/KGHAPPYShadows.ttf";
+import "../src/fonts/KGHAPPYSolid.ttf";
 
 //Components
-import Home from './components/landingpage/home'
-import Popular from './components/landingpage/popular'
-import Kenapa from './components/landingpage/kenapa'
-import Contact from './components/landingpage/contact'
+import Home from "./components/landingpage/home";
+import Popular from "./components/landingpage/popular";
+import Kenapa from "./components/landingpage/kenapa";
+import Contact from "./components/landingpage/contact";
 
 const App = () => {
   return (
-      <>
-        <Home />
-        <Popular />
-        <Kenapa />
-        <Contact />
-      </>
-  )
-}
-
+    <>
+      <Home />
+      <Popular />
+      <Kenapa />
+      <Contact />
+    </>
+  );
+};
 
 export default App;
